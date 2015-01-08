9756fd56770e0e3a40abb6d1b75b969dfd0d4ddb
Eric Jung <myst5829@gmail.com>
2015-01-08 11:12:23 +0900

    Removed the useless files (files for compilation)
    Removed the useless files (files for compilation)

--- WIZ550web_Firmware/WIZ550web_Boot/Debug/src/Ethernet/wizchip_conf.d
+++
@@ -1,9 +0,0 @@
-src/Ethernet/wizchip_conf.o: ../src/Ethernet/wizchip_conf.c \
- ../src/Ethernet/wizchip_conf.h ../src/Ethernet/W5500/w5500.h \
- D:\WIZnet\__Purple\WIZ550WEB\workspace\WIZ550web_Boot\src/Ethernet/wizchip_conf.h
-
-../src/Ethernet/wizchip_conf.h:
-
-../src/Ethernet/W5500/w5500.h:
-
-D:\WIZnet\__Purple\WIZ550WEB\workspace\WIZ550web_Boot\src/Ethernet/wizchip_conf.h:
