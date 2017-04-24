ce84c5140131eaa017ec60ebdabed9ab4e945edb
Kei <kei@wiznet.io>
2017-04-24 11:05:32 +0900

    Remove dependancy files & README Update

--- WIZ550web_Firmware/WIZ550web_HW_Rev1.1/WIZ550web_Boot/Debug/src/Ethernet/socket.d
+++
@@ -1,9 +0,0 @@
-src/Ethernet/socket.o: ../src/Ethernet/socket.c ../src/Ethernet/socket.h \
- D:\Wiznet\Firmware\WIZ550web_workspace_rev1.1_FW2.0.0\WIZ550web-WIZ550web_Rev1.1\WIZ550web-WIZ550web_Rev1.1\WIZ550web_Firmware\WIZ550web_HW_Rev1.1\WIZ550web_Boot\src/Ethernet/wizchip_conf.h \
- D:\Wiznet\Firmware\WIZ550web_workspace_rev1.1_FW2.0.0\WIZ550web-WIZ550web_Rev1.1\WIZ550web-WIZ550web_Rev1.1\WIZ550web_Firmware\WIZ550web_HW_Rev1.1\WIZ550web_Boot\src/Ethernet/W5500/w5500.h
-
-../src/Ethernet/socket.h:
-
-D:\Wiznet\Firmware\WIZ550web_workspace_rev1.1_FW2.0.0\WIZ550web-WIZ550web_Rev1.1\WIZ550web-WIZ550web_Rev1.1\WIZ550web_Firmware\WIZ550web_HW_Rev1.1\WIZ550web_Boot\src/Ethernet/wizchip_conf.h:
-
-D:\Wiznet\Firmware\WIZ550web_workspace_rev1.1_FW2.0.0\WIZ550web-WIZ550web_Rev1.1\WIZ550web-WIZ550web_Rev1.1\WIZ550web_Firmware\WIZ550web_HW_Rev1.1\WIZ550web_Boot\src/Ethernet/W5500/w5500.h:
--- WIZ550web_Firmware/WIZ550web_HW_Rev1.1/WIZ550web_Boot/Debug/system/src/newlib/_write.d
+++
@@ -1,4 +0,0 @@
-system/src/newlib/_write.o: ../system/src/newlib/_write.c \
- ../system/include/diag/Trace.h
-
-../system/include/diag/Trace.h:
